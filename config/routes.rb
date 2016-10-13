c3e86d6ddd63ef5fcc737bd3815b5002f225ac8c
trainj <laing.jay@gmail.com>
2016-10-12 22:23:17 -0400

    added home and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+    get 'about' => 'pages#about'
+        get 'contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
