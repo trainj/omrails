d736acde58435d4ef366ffa1f458932f81aa479f
trainj <laing.jay@gmail.com>
2016-10-27 15:14:33 -0400

    fixed admin mistake

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -12,6 +12,20 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+    permit_params :email, :admin
 
-
+    form do |f|
+        f.semantic_errors
+        f.inputs do
+            f.input :email
+            f.input :admin
+        end
+        f.actions
+    end
+    index do
+        id_column 
+        column :email
+        column :admin
+        actions
+    end
 end
