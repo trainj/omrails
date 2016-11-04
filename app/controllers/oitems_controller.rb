b1fe31d6236e670b5c86af0f487dfc00f50a4e67
trainj <laing.jay@gmail.com>
2016-11-03 23:09:24 -0400

    users works show doesnt

--- app/controllers/oitems_controller.rb
+++ app/controllers/oitems_controller.rb
@@ -2,73 +2,57 @@ class OitemsController < ApplicationController
   before_action :set_oitem, only: [:show, :edit, :update, :destroy]
 
   # GET /oitems
-  # GET /oitems.json
   def index
     @oitems = Oitem.all
   end
 
   # GET /oitems/1
-  # GET /oitems/1.json
   def show
+    @oitem = Oitem.find(params[:id])
   end
 
   # GET /oitems/new
   def new
-    @oitem = Oitem.new
+      @oitem = current_user.oitems.new
   end
 
   # GET /oitems/1/edit
   def edit
+      @oitem = current_user.oitems.find(params[:id])
   end
 
   # POST /oitems
-  # POST /oitems.json
   def create
-    @oitem = Oitem.new(oitem_params)
-
-    respond_to do |format|
-      if @oitem.save
-        format.html { redirect_to @oitem, notice: 'Oitem was successfully created.' }
-        format.json { render :show, status: :created, location: @oitem }
-      else
-        format.html { render :new }
-        format.json { render json: @oitem.errors, status: :unprocessable_entity }
-      end
+      @oitem = current_user.oitems.new(oitem_params)
+    if @oitem.save
+      redirect_to @oitem, notice: 'Oitem was successfully created.' 
+    else
+      render :new 
     end
   end
 
   # PATCH/PUT /oitems/1
-  # PATCH/PUT /oitems/1.json
   def update
-    respond_to do |format|
-      if @oitem.update(oitem_params)
-        format.html { redirect_to @oitem, notice: 'Oitem was successfully updated.' }
-        format.json { render :show, status: :ok, location: @oitem }
-      else
-        format.html { render :edit }
-        format.json { render json: @oitem.errors, status: :unprocessable_entity }
-      end
+      @oitem = current_user.oitems.find(params[:id])
+    if @oitem.update(oitem_params)
+     redirect_to @oitem, notice: 'Oitem was successfully updated.' 
+    else
+     render :edit 
     end
   end
 
   # DELETE /oitems/1
-  # DELETE /oitems/1.json
   def destroy
+      @oitem = current_user.oitems.find(params[:id])
     @oitem.destroy
-    respond_to do |format|
-      format.html { redirect_to oitems_url, notice: 'Oitem was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to oitems_url, notice: 'Oitem was successfully destroyed.' 
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_oitem
-      @oitem = Oitem.find(params[:id])
-    end
+    
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def oitem_params
-      params.require(:oitem).permit(:user_id, :title, :url, :text)
+      params.require(:oitem).permit(:title, :url, :text)
     end
 end
